9db0c1c9f4c0cbd05973758238a24268702d8fcc
FadhilYTProgrammer <fadhil18022007@gmail.com>
2023-02-17 15:02:59 +0700

    penyempurnaan

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -4,17 +4,14 @@
     <div class="w-96 justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
         <a href="{{ url('/admin/create') }}" class="btn btn-sm btn-primary">tambah</a>
         <span class="fs-5 fw-bold">List Buku</span>
-        <div class="md:hidden mt-2 mx-auto">
-            {{ $data->links('pagination::tailwind') }}
-        </div>
+        <div class="md:hidden mx-auto mt-4">{{ $data->links('pagination::tailwind') }}</div>
     </div>
 </div>
 
-<div class="hidden md:block mx-10 mt-0 md:mt-2">{{ $data->links('pagination::tailwind') }}</div>
-
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+<div class="hidden md:block mx-4 mt-4">{{ $data->links('pagination::tailwind') }}</div>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-2 md:my-7">
     @foreach ($data as $item)
-    <div class="card mt-2 w-96 bg-base-100 shadow-xl mx-auto">
+    <div class="border card mt-2 w-96 bg-base-100 shadow-xl mx-auto">
         <div class="card-body">
             <span class="badge badge-outline">{{$item->statuss}}</span>
             <h2 class="card-title">{{$item->user}}</h2>
--- resources/views/task/tamu.blade.php
+++ resources/views/task/tamu.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 @section('main')
-<div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 380px;">
+<div class="border rounded mt-3 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 380px;">
     <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
         <span class="fs-5 fw-semibold">List Buku</span>
-        <div class="hidden md:block">{{ $data->links('pagination::tailwind') }}</div>
+        <div class="md:hidden mx-auto mt-4">{{ $data->links('pagination::tailwind') }}</div>
     </div>
 </div>
 
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    <div class="md:hidden mx-auto mt-2">{{ $data->links('pagination::tailwind') }}</div>
+<div class="hidden md:block mx-4 mt-4">{{ $data->links('pagination::tailwind') }}</div>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4 md:my-7">
     @foreach ($data as $item)
-    <div class="card mt-0 md:mt-2 w-96 bg-base-100 shadow-xl mx-auto">
+    <div class="card border mt-0 md:mt-2 w-96 bg-base-100 shadow-xl mx-auto">
         <div class="card-body">
             <span class="badge badge-outline">{{$item->statuss}}</span>
             <h2 class="card-title">{{$item->user}}</h2>
@@ -18,6 +18,6 @@
         </div>
     </div>
     @endforeach
-    
+
 </div>
 @endsection
